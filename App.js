1f90f9c4b149e5413e5d50aa78687eca55cffb1a
Lana <44567161+lanasvet12@users.noreply.github.com>
2020-03-12 18:03:12 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+
+export default class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      error: "",
+
+      comment: {
+        name: "",
+        message: ""
+      }
+    };
+
+    // bind context to methods
+    this.handleFieldChange = this.handleFieldChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  /**
+   * Handle form input field changes & update the state
+   */
+  handleFieldChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({
+      ...this.state,
+      comment: {
+        ...this.state.comment,
+        [name]: value
+      }
+    });
+  };
+
+  /**
+   * Form submit handler
+   */
+  onSubmit(e) {
+    // prevent default form submission
+    e.preventDefault();
+    //...
+  }
+
+  renderError() {
+    return this.state.error ? (
+      <div className="alert alert-danger">{this.state.error}</div>
+    ) : null;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <form method="post" onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <input
+              onChange={this.handleFieldChange}
+              value={this.state.comment.name}
+              className="form-control"
+              placeholder="😎 Your Name"
+              name="name"
+              type="text"
+            />
+          </div>
+
+          <div className="form-group">
+            <textarea
+              onChange={this.handleFieldChange}
+              value={this.state.comment.message}
+              className="form-control"
+              placeholder="🤬 Your Comment"
+              name="message"
+              rows="5"
+            />
+          </div>
+
+          {this.renderError()}
+
+          <div className="form-group">
+            <button disabled={this.state.loading} className="btn btn-primary">
+              Comment ➤
+            </button>
+          </div>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
